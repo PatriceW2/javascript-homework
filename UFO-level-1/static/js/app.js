c4a1be2653e509e8369c01b7bbe02188bac1c20e
PatriceW <tolmie2@gmail.com>
2021-04-01 19:33:18 -0700

    getting started

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,10 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//find the table in the html
+let tbody = d3.select("tbody")
+
+//find the button and the form in the html
+
+let button = d3.select("#filter-btn")
+let form = d3.select("#form")
