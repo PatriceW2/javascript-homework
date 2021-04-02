cbb00291b6d554f65ea724a179189e9f1f282e1e
PatriceW <tolmie2@gmail.com>
2021-04-01 19:55:39 -0700

    added table to html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,11 +1,29 @@
 // from data.js
 var tableData = data;
 
+//console.log(tableData)
+
 // YOUR CODE HERE!
 //find the table in the html
 let tbody = d3.select("tbody")
+
+
 
 //find the button and the form in the html
 
 let button = d3.select("#filter-btn")
 let form = d3.select("#form")
+
+//fill html table
+
+tableData.forEach(ufoData => {
+    let row = tbody.append("tr");
+    Object.values(ufoData).forEach(value => {
+        let cell = row.append("td");
+        cell.text(value);
+    })
+
+})
+   
+
+
